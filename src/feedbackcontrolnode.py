bdc2de9dfb9ab436364b2d21dd53baa5fa62d5f1
Nikolas Pardoe <pardo020@umn.edu>
2022-01-09 10:24:21 -0600

    Add gimbal yaw control. Untested draft.

--- src/feedbackcontrolnode.py
+++ src/feedbackcontrolnode.py
@@ -20,15 +20,18 @@ deadzone_position = 0.0 #deadzone on controlling position in frame
 
 size_gain = 2
 yaw_gain = 1
-pitch_gain = -100
+gimbal_pitch_gain = -100
+gimbal_yaw_gain = 100
 yaw_mode = True
 traverse_gain = 2
 
 limit_speed = 2
 limit_yawrate = .75
 limit_pitchchange = 50
+limit_yawchange = 50
 
 pitchcommand = 1500
+yawcommand = 1500
 
 yaw = 0
  
@@ -64,15 +67,20 @@ def yaw_callback(pose):
 
 def boundingbox_callback(box):
     global horizontalerror, verticalerror, sizeerror
-    global time_lastbox, pitchcommand
-    #positive errors right, down, forward
+    global time_lastbox, pitchcommand, yawcommand
+    #positive errors left, up, forward
     horizontalerror = .5-box.center.x
     verticalerror = .5-box.center.y
     sizeerror = setpoint_size - max(box.size_x, box.size_y)
     time_lastbox = rospy.Time.now()
-    pitchdelta = verticalerror * pitch_gain
+    pitchdelta = verticalerror * gimbal_pitch_gain
     pitchdelta = min(max(pitchdelta,-limit_pitchchange),limit_pitchchange)
     pitchcommand += pitchdelta
+    pitchcommand = min(max(pitchcommand,1000),2000)
+    yawdelta = verticalerror * gimbal_yaw_gain
+    yawdelta = min(max(yawdelta,-limit_yawchange),limit_yawchange)
+    yawcommand += yawdelta
+    yawcommand = min(max(yawcommand,1000),2000)
     return
 
 def dofeedbackcontrol():
@@ -102,10 +110,11 @@ def dofeedbackcontrol():
                 fspeed = sizeerror * size_gain
             else:
                 fspeed = 0
-            yawrate = horizontalerror * yaw_gain
+            # yawrate = horizontalerror * yaw_gain #commented out when gimbal yaw is active
+            yawrate = ((yawcommand - 1500)/1000)*yaw_gain*.75 #.75 multiplier included here for now, should be pulled out to gain later
             hspeed = horizontalerror * traverse_gain
-            pitchdelta = verticalerror * pitch_gain
-            pitchdelta = min(max(pitchdelta,-limit_pitchchange),limit_pitchchange)
+            # pitchdelta = verticalerror * gimbal_pitch_gain
+            # pitchdelta = min(max(pitchdelta,-limit_pitchchange),limit_pitchchange)
             #pitchcommand += pitchdelta
             
             #bound controls to ranges
@@ -113,7 +122,9 @@ def dofeedbackcontrol():
             hspeed = min(max(hspeed,-limit_speed),limit_speed)
             yawrate = min(max(yawrate,-limit_yawrate),limit_yawrate)
             pitchcommand = min(max(pitchcommand,1000),2000)
-            rcmsg.channels[7] = int(pitchcommand)
+            yawcommand = min(max(yawcommand,1000),2000)
+            rcmsg.channels[7] = int(pitchcommand) #send pitch command on channel 8
+            rcmsg.channels[6] = int(yawcommand) #send yaw command on channel 7
             #assign to messages, publish
             if yaw_mode:
                 twistmsg.linear.x = math.cos(yaw)*fspeed
@@ -128,6 +139,7 @@ def dofeedbackcontrol():
             rcpub.publish(rcmsg)
         elif time_lastbox != None and (rospy.Time.now() - time_lastbox > rospy.Duration(5)):
             pitchcommand = 1500
+            yawcommand = 1500
 
         
         rate.sleep()
